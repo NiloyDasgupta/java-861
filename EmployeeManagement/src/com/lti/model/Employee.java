a1b3b3088239ec5635734d0841a06c8c1713bff4
niloy <niloy@MSI>
2020-10-15 12:41:47 +0530

    added Employee class

---
+++ EmployeeManagement/src/com/lti/model/Employee.java
@@ -0,0 +1,28 @@
+package com.lti.model;
+
+public class Employee {
+	private int employeeId;
+	private String employeeName;
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public Employee(int employeeId) {
+		super();
+		this.employeeId = employeeId;
+	}
+	public Employee(int employeeId, String employeeName) {
+		super();
+		this.employeeId = employeeId;
+		this.employeeName = employeeName;
+	}
+	
+}
